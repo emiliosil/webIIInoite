bee1958d50b9be9d0f8c4bbd26a8067691ff3190
Emílio Silva Corrêa <emiliosilva404@gmail.com>
2021-09-08 22:05:06 -0300

    Atividade calculadora

---
+++ calculadora/src/calculadora/Principal.java
@@ -0,0 +1,28 @@
+package calculadora;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		float num1;
+		float num2;
+		
+		try {
+		System.out.print("Digite o primeiro número");
+		num1 = teclado.nextFloat();
+		}catch (Exception ex) {
+			teclado = new Scanner(System.in);
+			num1 = 0;
+		}
+		
+		System.out.print("Digite o segundo número");
+		num2 = teclado.nextFloat();
+		
+		Calculadora calc = new Calculadora();
+		System.out.printf("A soma é: %.2f", calc.somar(num1, num2));
+	}
+	
+}
