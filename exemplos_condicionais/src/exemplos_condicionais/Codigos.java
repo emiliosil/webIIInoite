d111cb8afd800b7be121fbbda3361d8e1399e5aa
Emílio Silva Corrêa <emiliosilva404@gmail.com>
2021-09-15 22:01:03 -0300

    Aprendendo if/else

---
+++ exemplos_condicionais/src/exemplos_condicionais/Codigos.java
@@ -0,0 +1,91 @@
+package exemplos_condicionais;
+
+//import java.util.Random;
+
+//Random rand = new Random();
+//int valor1 = rand.nextInt(4);
+//int valor2 = rand.nextInt(4);
+//int valor3 = rand.nextInt(4);
+//int valor4 = rand.nextInt(4);
+//
+//boolean comp1 = valor1 == valor2;
+//boolean comp2 = valor3 == valor4;                               
+//
+//boolean comp3 = valor3 > valor2;
+//boolean comp4 = valor4 < valor1;
+//
+////System.out.printf("É verdade que valor 1 é igual ao valor 2: %b\n",comp1);
+//
+////System.out.printf("É verdade que valor 3 é igual ao valor 4: %b\n",comp2);
+//
+////System.out.printf("É verdade que valor 3 é maior que valor 2: %b\n",comp3);
+//
+//boolean logico1 = comp1 || comp4;
+//boolean logico2 = comp3 && comp2;
+////
+////System.out.printf("O lógico 1 é verdadeiro %b\n",logico1);
+////System.out.printf("O lógico 2 é verdadeiro %b\n",logico2);
+//
+//boolean souAlunoQI = true; 
+//boolean estouMatriculadoNaAssis = true;
+//boolean estouMatriculadoNaNoite = true; 
+//boolean estouMatriculadoNaTurmaDoMelhorProfessorDaQI = true;
+//
+//// Posso usar o PC da escola
+//if(souAlunoQI) {
+//	System.out.println("Pode usar o PC da escola");
+//}else {
+//	System.out.println("NÃO pode usar o PC da escola");
+//}
+//
+//// Posso participar da aula de web III
+//if(souAlunoQI && estouMatriculadoNaAssis && estouMatriculadoNaNoite && estouMatriculadoNaTurmaDoMelhorProfessorDaQI) {
+//	System.out.println("Você pode assistir a aula de web III!!!");
+//}else {
+//	System.out.println("Você NÃO pode assistir a aula de web III!!!");
+//}
+//
+//if(souAlunoQI || estouMatriculadoNaAssis || estouMatriculadoNaNoite || estouMatriculadoNaTurmaDoMelhorProfessorDaQI) {
+//	System.out.println("Você pode assistir a aula de web III!!!");
+//}else {
+//	System.out.println("Você NÃO pode assistir a aula de web III!!!");
+//}
+//
+//if(estouMatriculadoNaAssis && estouMatriculadoNaNoite && estouMatriculadoNaTurmaDoMelhorProfessorDaQI) {
+//	System.out.println("Você pode assistir a aula de web III!!!");
+//}else {
+//	System.out.println("Você NÃO pode assistir a aula de web III!!!");
+//}
+//
+//}
+
+
+//System.out.println("Digite o primeiro número:");
+//float num1 = teclado.nextFloat();
+//System.out.println("Digite o segundo número:");
+//float num2 = teclado.nextFloat();
+//
+//float maior = maiorNumero(num1, num2);
+//
+//if (maior == 0) {
+//	System.out.println("Os números são iguais!!!");
+//} else {
+//	System.out.printf("O maior número é o %.1f\n", maior);
+//}
+//
+//boolean num1Positivo = ePositivo(num1);
+//boolean num2Positivo = ePositivo(num2);
+//
+//System.out.printf("O primeiro número é positivo: %b\n", num1Positivo);
+//System.out.printf("O segundo número é positivo: %b\n", num2Positivo);
+
+//System.out.println("Digite uma letra:");
+//String letra = teclado.next();
+//
+//boolean eVogal = eVogal(letra);
+//
+//if(eVogal) {
+//	System.out.printf("A letra %s é uma vogal!!!",letra);
+//}else {
+//	System.out.printf("A letra %s NÃO é uma vogal!!!",letra);
+//}
---
+++ exemplos_condicionais/src/exemplos_condicionais/Principal.java
@@ -0,0 +1,48 @@
+package exemplos_condicionais;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Digite o seu sexo: M ou F");
+		String sexo = teclado.next();
+		
+		String resultado = verificarSexo(sexo);
+		System.out.printf("Seu sexo é %s\n",resultado);
+	}
+
+	private static float maiorNumero(float num1, float num2) {
+		// Primeiro if
+		if (num1 == num2) {
+			return 0;
+		}
+
+		// Segundo if
+		if (num1 > num2) {
+			return num1;
+		}
+
+		return num2;
+	}
+
+	private static boolean ePositivo(float num) {
+		return num > -1;
+	}
+	
+	private static boolean eVogal(String letra) {
+		return letra.equalsIgnoreCase("a")  || letra.equalsIgnoreCase("e")  || letra.equalsIgnoreCase("i") || letra.equalsIgnoreCase("o") || letra.equalsIgnoreCase("u");
+	}
+	
+	private static String verificarSexo(String sexo) {
+		if(sexo.equalsIgnoreCase("M")) {
+			return "Masculino";
+		}else if(sexo.equalsIgnoreCase("F")) {
+			return "Feminino";
+		}else {
+			return "Sexo inválido";
+		}
+	}
+}
