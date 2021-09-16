61fad14f02ebfa43c06ad5675b14e232cf0be215
Emílio Silva Corrêa <emiliosilva404@gmail.com>
2021-09-16 20:48:09 -0300

    Atividade 2

---
+++ atividade2/src/atividade2/Main.java
@@ -0,0 +1,25 @@
+package atividade2;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner ent =  new Scanner ( System.in);
+        int ano;
+        
+        System.out.println ( " Digite um ano para saber se é bissexto " );
+        ano = ent . nextInt ();
+        
+        // se o ano para maior que 400
+        if (ano %  400  ==  0 ) {
+            System.out.println(ano +" é bissexto.");
+        // se o ano para menor que 400
+        } else  if ((ano %  4  ==  0 ) && (ano % 100  ==  0 )){
+            System.out.println(ano +  " é bissexto. " );
+        } else {
+            System.out.println(ano +  " não é bissexto " );
+        }
+	}
+
+}
